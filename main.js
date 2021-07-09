0e2751566a1a8c7e7b0c2b4188f6728fe2f4392d
angel-vg <angel.vkz@outlook.com>
2021-07-08 20:45:31 -0500

    Player movement implement. Rendering Ok. Test Controllers

--- main.js
+++ main.js
@@ -6,18 +6,35 @@
 import { World } from "./src/map/World.js"
 import { mapGrid } from "./src/map/GridExample.js"
 import { context, width, height } from "./src/globals/globals.js"
+import { Player } from "./src/player/Player.js"
 
-    
+let cell_size = 30,
+    rows      = 11,
+    cols      = 15,
+    mapWidth  = cols * cell_size,
+    mapHeight = rows * cell_size;
+
 
 let grid = new World(mapGrid, context);
-
+let player = new Player({
+    x: mapWidth  / 2,
+    y: mapHeight / 2,
+    r: 5,
+    walkSpeed: 3.0,
+    turnSpeed: 0.05,
+    context,
+})
 
 
 render();
 function render()
 {
 
-    context.clearRect(0,0,width, height)
-    grid.render()
+    context.clearRect(0,0,width, height);
+
+    grid.render();
+    player.update();
+    player.render();
+
     requestAnimationFrame(render);
 }
--- src/player/Player.js
+++ src/player/Player.js
@@ -1,4 +1,8 @@
 
+/**
+ * @author Miguel Angel Vega Garcia
+ * @date 7/8/2021
+ */
 export class Player
 {
     constructor(params)
@@ -7,10 +11,74 @@ export class Player
         this.y = params.y;
         this.r = params.r;
         this.turnDirection = 0;    //  -1 0 1      left      -  nothing - right 
+        this.turnSpeed = params.turnSpeed;
         this.walkDirection = 0;    //  -1 0 1      upwards    - nothing - backwards
-        this.rotationAngle = Math.PI * 2;
+        this.walkSpeed = params.walkSpeed;
+        this.rotationAngle = Math.PI * 2;    
+        this.context = params.context;
 
-        this.walkSpeed = 3.0;
-        this.turnSpeed = 0.05;
+        this.startControls();
+    }
+
+    startControls()
+    {
+        window.onkeydown = e =>
+        {
+            switch(e.key)
+            {
+                case "ArrowUp"   : this.walkDirection = +1; break;
+                case "ArrowDown" : this.walkDirection = -1; break;
+                case "ArrowLeft" : this.turnDirection = -1; break;
+                case "ArrowRight": this.turnDirection = +1; break;
+                default:;
+            }
+        }
+
+        window.onkeyup = e =>
+        {
+            switch(e.key)
+            {
+                case "ArrowUp"   : this.walkDirection = 0; break; 
+                case "ArrowDown" : this.walkDirection = 0; break;
+                case "ArrowLeft" : this.turnDirection = 0; break;
+                case "ArrowRight": this.turnDirection = 0; break;
+                default:;
+            }
+        }
+
+        console.log(this.turnDirection);
+
+    }
+
+    render()
+    {
+        // Circle Player 
+        this.context.beginPath();
+        this.context.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+        this.context.fillStyle = "red";
+        this.context.fill()
+        this.context.closePath();
+        
+        //Direction Line render
+        let x = this.x + Math.cos(this.rotationAngle) * 20;
+        let y = this.y + Math.sin(this.rotationAngle) * 20;
+
+        this.context.beginPath();
+        this.context.moveTo(this.x, this.y);
+        this.context.lineTo(x, y);
+        this.context.stroke();
+        this.context.closePath();
+
+    }
+
+
+    update()
+    {
+        this.rotationAngle += this.turnDirection * this.turnSpeed;
+
+        let step = this.walkDirection * this.walkSpeed;
+        this.x += Math.cos(this.rotationAngle) * step;
+        this.y += Math.sin(this.rotationAngle) * step;
+
     }
 }
